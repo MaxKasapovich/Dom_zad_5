f8fe4516953a587e519aca0b36e23a02380afc4b
Max Kasapovich <crazywarm@ukr.net>
2022-04-21 12:59:13 +0300

    Annotation

--- src/com/company/CreditCard.java
+++ src/com/company/CreditCard.java
@@ -13,7 +13,7 @@ public class CreditCard {
                 System.out.println("Dear " + owner + " congratulations, you are using a normal card");
                 break;
             case "МИР":
-                System.out.println(owner + " fuck you and your russian ship!!");
+                System.out.println(owner + " f@ck you and your russian ship!!");
                 break;
             default:
                 System.out.println("Unknown card type: " + type);
